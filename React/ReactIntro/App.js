929aabce749b1a90c84a24dd2556ae1dbc5a424b
BIT student <classroom@bgit.rs>
2018-03-18 22:30:02 +0100

    promise

--- React/ReactIntro/App.js
+++ React/ReactIntro/App.js
@@ -34,3 +34,138 @@ var App = (props) => {
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App title={"Caooooooo"} fruits={["Apple", "Dog", "Telephone"]}/>,rootElement);
+
+
+//-----------------------------------------PROMISE-----------------------------------------------------
+
+function withPromise(title) {
+
+    // uzimamo nasumično broj od 1000 do 2000
+    var wait = Math.floor(Math.random() * 1000);
+
+    var promise = new Promise(function(resolve, reject) {
+
+        if (typeof wait !== "number") {
+            reject("Wait mora biti broj")
+        }
+
+        setTimeout(function() {
+             resolve(title + 'ok   ' + typeof title + '  ');
+             console.log(wait + '   '+title);
+        }, wait);
+
+    });
+
+
+    console.log('aaaaaaaaaaa')
+    return promise;
+}
+
+
+// više ne prosleđujemo callback funkciju
+withPromise('1').then(function(data) {
+    console.log(data)
+    return withPromise(data)
+}).then(function(data) {
+    console.log(data)
+    return withPromise(data)
+}).then(function(data) {
+    console.log(data);
+});
+
+
+
+//-----------------------------------------------------------------------------
+function withPromise(title) {
+
+    // uzimamo nasumično broj od 1000 do 2000
+    var wait = Math.floor(Math.random() * 1000);
+
+    var promise = new Promise(function(resolve, reject) {
+
+        if (typeof wait !== "number") {
+            reject("Wait mora biti broj")
+        }
+
+        setTimeout(function() {
+            return resolve(title + ": " + (wait / 1000) + " sekundi");
+        }, wait);
+
+    });
+
+    return promise;
+}
+
+
+// više ne prosleđujemo callback funkciju
+withPromise("1").then(function(data) {
+    console.log(data + "\n");
+}).catch(function(error) {
+        // obrada greške
+});
+
+// jedan od načina vezivanja više poziva
+Promise.all([withPromise("1"), withPromise("2"), withPromise("3")])
+    .then(function(values) {
+
+        for (data in values) {
+            console.log(values[data] + "\n");
+        }
+    });
+
+// ko će brže vratiti rješenje?
+Promise.race([withPromise("1"), withPromise("2"), withPromise("3")])
+    .then(function(data) {
+        console.log("Ja sam najbrži: " + data + "\n");
+    });
+
+    //-------------------------------------------------------------------------------------------
+    var isMomHappy = true;
+
+// Promise
+var willIGetNewPhone = new Promise(
+    function (resolve, reject) {
+        if (isMomHappy) {
+            var phone = {
+                brand: 'Samsung',
+                color: 'black'
+            };
+            resolve(phone); // fulfilled
+        } else {
+            var reason = new Error('mom is not happy');
+            reject(reason); // reject
+        }
+
+    }
+);
+
+// 2nd promise
+var showOff = function (phone) {
+    return new Promise(
+        function (resolve, reject) {
+            var message = 'Hey friend, I have a new ' +
+                phone.color + ' ' + phone.brand + ' phone';
+
+            resolve(message);
+        }
+    );
+};
+
+// call our promise
+var askMom = function () {
+    console.log('before asking Mom'); // log before
+    willIGetNewPhone
+        .then(showOff)
+        .then(function (value) {
+            console.log(value);
+	     // output: { brand: 'Samsung', color: 'black' }
+        })
+        .catch(function (error) {
+            console.log(error.message);
+	     // output: 'mom is not happy'
+        });
+    
+    console.log('after asking mom'); // log after
+};
+
+askMom();
